09713bf95c8449198a2eba6ce33f5c6f84194ea4
Jake Morrison <jake@cogini.com>
2019-02-28 15:17:13 -0800

    Add mix_deploy

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule MixSystemdDeploy.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:distillery, "~> 2.0"},
+      {:mix_deploy, "~> 0.1.0"},
     ]
   end
 
