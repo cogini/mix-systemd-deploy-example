4175ac680c7560427b05cb8eac98474bb203762a
Jake Morrison <jake@cogini.com>
2019-04-20 16:12:35 +0800

    Update migration command

--- lib/mix_systemd_deploy/tasks/migrate.ex
+++ lib/mix_systemd_deploy/tasks/migrate.ex
@@ -1,11 +1,13 @@
 defmodule MixSystemdDeploy.Tasks.Migrate do
-  @moduledoc false
-  # CHANGEME
-  @app :cloud_native
+  @moduledoc "Mix task to run Ecto database migrations"
+
+  # CHANGEME: Name of app as used by Application.get_env
+  @app :mix_systemd_deploy
+  # CHANGEME: Name of app repo module
   @repo_module MixSystemdDeploy.Repo
 
-  def migrate(_args) do
-    # Configure
+  def run(_args) do
+    # CHANGEME: Runtime configuration method/file
     Mix.Releases.Config.Providers.Elixir.init(["/etc/mix-systemd-deploy/config.toml"])
     # Mix.Releases.Config.Providers.Elixir.init(["/etc/mix-systemd-deplouy/config.exs"])
 
@@ -20,17 +22,17 @@ defmodule MixSystemdDeploy.Tasks.Migrate do
       IO.puts "==> Started #{app}: #{inspect res}"
     end
 
-    # Start the repo
+    # Start repo
     IO.puts "==> Starting repo"
     {:ok, _pid} = apply(@repo_module, :start_link, [[pool_size: 1, log: :info, log_sql: true]])
 
-    # Run the migrations for the repo
+    # Run migrations for the repo
     IO.puts "==> Running migrations"
     priv_dir = Application.app_dir(@app, "priv")
     migrations_dir = Path.join([priv_dir, "repo", "migrations"])
 
     opts = [all: true]
-    # CHANGEME
+    # CHANGEME: Name of repo module
     pool = MixSystemdDeploy.Repo.config[:pool]
     if function_exported?(pool, :unboxed_run, 2) do
       pool.unboxed_run(@repo_module, fn -> Ecto.Migrator.run(@repo_module, migrations_dir, :up, opts) end)
--- rel/commands/migrate.sh
+++ rel/commands/migrate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-release_ctl eval "MixSystemdDeploy.Tasks.migrate(:init.get_plain_arguments())"
+release_ctl eval --mfa "MixSystemdDeploy.Tasks.Migrate.run/1" --argv -- "$@"
